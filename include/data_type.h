6cc4086c6cb82d186429a4978c56721ce9ab5aab
zengxiaoliang <zengxiaoliang@holomatic.com>
2022-03-12 10:55:06 +0800

    Add: Init Version
    Why: Init Version
    
    How: Init Version

---
+++ include/data_type.h
@@ -0,0 +1,15 @@
+#ifndef DATA_TYPE_H_
+#define DATA_TYPE_H_
+
+typedef signed char sint8;
+typedef unsigned char uint8;
+typedef short sint16;
+typedef unsigned short uint16;
+typedef int sint32;
+typedef unsigned int uint32;
+typedef long long sint64;
+typedef unsigned long long uint64;
+typedef float float32;
+typedef double float64;
+
+#endif
---
+++ src/tsm_worker.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "data_type.h"
+
+sint32 main() 
+{
+  sint32 a = 0;
+
+  return 0;
+}
